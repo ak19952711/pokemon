6389fafe21f73d7fa2b121c85e2f9d7d47bf9bd3
Akshay Kumar <akshku01@in.ibm.com>
2021-06-14 02:20:26 +0530

    'ssds'

---
+++ src/main/java/com/pokeworld/pokemon/PokemonApplication.java
@@ -0,0 +1,14 @@
+package com.pokeworld.pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonApplication {
+
+	public static void main(String[] args) {
+		System.out.println("RANNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN");
+		SpringApplication.run(PokemonApplication.class, args);
+	}
+
+}
